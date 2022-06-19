b2991323fba4337025a9fcce27b69aab22138af7
Ari Kurniawan <arikurnia2601@gmail.com>
2022-06-19 23:42:10 +0700

    Added setup makefile, extract vendor file, and proprietary list file.

---
+++ setup-makefiles.sh
@@ -0,0 +1,32 @@
+set -e
+
+export INITIAL_COPYRIGHT_YEAR=2015
+
+# Required!
+DEVICE=j1acevelte
+VENDOR=samsung
+
+# Load extractutils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
+LINEAGE_ROOT="$MY_DIR"/../../..
+
+HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
+    exit 1
+fi
+. "$HELPER"
+
+# Initialize the helper
+setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT"
+
+# Copyright headers and guards
+write_headers
+
+# The standard blobs
+write_makefiles "$MY_DIR"/proprietary-files.txt
+
+# We are done!
+write_footers
